5119edb22fcd75a2d4c5b8efa7db4a6611da6315
tomiherrero <tomas.herrero@hotmail.es>
2020-11-08 09:28:48 -0300

    se cambia el nombre del array idveterinarias en voluntario, se cambia nombres de la entity de usuarioGETDTO

--- src/main/java/com/tejag/cshsoftware/apirest/models/service/UsuarioRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.tejag.cshsoftware.apirest.models.service;
-
-import com.tejag.cshsoftware.apirest.models.entity.RoleUsuario;
-
-public interface UsuarioRoleService {
-	
-	public RoleUsuario getById(Long id);
-	
-}
--- src/main/java/com/tejag/cshsoftware/apirest/models/service/dto/UsuarioRoleDTOService.java
+++
@@ -1,8 +0,0 @@
-package com.tejag.cshsoftware.apirest.models.service.dto;
-
-import com.tejag.cshsoftware.apirest.models.dto.UsuarioRoleDTO;
-
-public interface UsuarioRoleDTOService {
-
-	public UsuarioRoleDTO getById(Long id);
-}
